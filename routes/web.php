b073bcd437dbae9603e6d032dcf85eaff49f868d
bagasPrabowo <bagasp1504555@gmail.com>
2020-07-09 19:21:25 +0700

    vote database

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+
 Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
     \UniSharp\LaravelFilemanager\Lfm::routes();
 });
