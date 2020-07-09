b6d1122982d4649c654fb317776be9a872ea7ad7
Zaki27 <zakinugraha27@gmail.com>
2020-07-09 13:10:30 +0700

    auth_login_register

--- routes/web.php
+++ routes/web.php
@@ -12,26 +12,30 @@
 */
 
 Route::get('/', function () {
-    return view('lte_layout.master');
+    return view('welcome');
 });
 
-route::get('/pertanyaan', 'QuestionsController@index')->name('pertanyaan.index');
+Route::group(['middleware' => 'auth'], function(){
+    route::get('/pertanyaan/create', 'QuestionsController@create')->name('pertanyaan.create');
 
-route::get('/pertanyaan/create', 'QuestionsController@create')->name('pertanyaan.create');
+    route::post('/pertanyaan', 'QuestionsController@store')->name('pertanyaan.store');
+    
+    route::get('/pertanyaan/{id}', 'QuestionsController@show')->name('pertanyaan.show');
+    
+    route::get('/pertanyaan/{id}/edit', 'QuestionsController@edit')->name('pertanyaan.edit');
+    
+    route::put('/pertanyaan/{id}', 'QuestionsController@update')->name('pertanyaan.update');
+    
+    route::delete('/pertanyaan/{id}', 'QuestionsController@delete')->name('pertanyaan.delete');
+    
+    route::get('/jawaban/{pertanyaan_id}', 'AnswersController@index')->name('jawaban.index');
+    
+    route::post('/jawaban/{pertanyaan_id}','AnswersController@store')->name('jawaban.store');
+});
 
-route::post('/pertanyaan', 'QuestionsController@store')->name('pertanyaan.store');
+Route::get('/pertanyaan', 'QuestionsController@index')->name('pertanyaan.index');
 
-route::get('/pertanyaan/{id}', 'QuestionsController@show')->name('pertanyaan.show');
 
-route::get('/pertanyaan/{id}/edit', 'QuestionsController@edit')->name('pertanyaan.edit');
-
-route::put('/pertanyaan/{id}', 'QuestionsController@update')->name('pertanyaan.update');
-
-route::delete('/pertanyaan/{id}', 'QuestionsController@delete')->name('pertanyaan.delete');
-
-route::get('/jawaban/{pertanyaan_id}', 'AnswersController@index')->name('jawaban.index');
-
-route::post('/jawaban/{pertanyaan_id}','AnswersController@store')->name('jawaban.store');
 
 Auth::routes();
 
